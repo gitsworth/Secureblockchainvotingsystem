cb1f855e9b083cf5d773fdf29e78ddabdcefb24a
gitsworth <satwiksnaik06@gmail.com>
2025-12-24 19:39:50 +0530

    Update wallet.py

--- wallet.py
+++ wallet.py
@@ -1,29 +1,21 @@
 import ecdsa
-import binascii
+import base64
 
 def generate_key_pair():
-    # Generate SECP256k1 keys (Bitcoin standard)
     sk = ecdsa.SigningKey.generate(curve=ecdsa.SECP256k1)
-    pk = sk.get_verifying_key()
-    return (
-        binascii.hexlify(sk.to_string()).decode(),
-        binascii.hexlify(pk.to_string()).decode()
-    )
+    pk = sk.verifying_key
+    return sk.to_string().hex(), pk.to_string().hex()
 
 def sign_transaction(private_key_hex, message):
     try:
-        sk_bytes = binascii.unhexlify(private_key_hex)
-        sk = ecdsa.SigningKey.from_string(sk_bytes, curve=ecdsa.SECP256k1)
-        signature = sk.sign(message.encode())
-        return binascii.hexlify(signature).decode()
+        sk = ecdsa.SigningKey.from_string(bytes.fromhex(private_key_hex), curve=ecdsa.SECP256k1)
+        return sk.sign(message.encode()).hex()
     except:
         return None
 
 def verify_signature(public_key_hex, message, signature_hex):
     try:
-        pk_bytes = binascii.unhexlify(public_key_hex)
-        sig_bytes = binascii.unhexlify(signature_hex)
-        pk = ecdsa.VerifyingKey.from_string(pk_bytes, curve=ecdsa.SECP256k1)
-        return pk.verify(sig_bytes, message.encode())
+        pk = ecdsa.VerifyingKey.from_string(bytes.fromhex(public_key_hex), curve=ecdsa.SECP256k1)
+        return pk.verify(bytes.fromhex(signature_hex), message.encode())
     except:
         return False
